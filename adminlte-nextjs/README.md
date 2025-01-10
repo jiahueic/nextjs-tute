9b6f3a0204ec3914e85087df1652578d838b0da9
jiahueic <jiahueicheah87@gmail.com>
2025-01-10 13:05:32 +0800

    dashboard 1 fail

--- adminlte-nextjs/README.md
+++ adminlte-nextjs/README.md
@@ -2,4 +2,5 @@
 
 ```sh
 npm install adminlte --save
+npm i bootstrap@5.3.3
 ```
--- adminlte-nextjs/app/(components)/Dashboard.jsx
+++ adminlte-nextjs/app/(components)/Dashboard.jsx
@@ -1,100 +1,19 @@
 import Head from "next/head";
 import Script from "next/script";
+import Header from "./Header";
+import MiniDashboard from "./MiniDashboard";
+import Menu from "./Menu";
+import Footer from "./Footer";
 
 const Dashboard = () => {
   return (
     <>
-      <Head>
-        <title>AdminLTE Dashboard</title>
-        <meta name="description" content="AdminLTE dashboard in Next.js" />
-        {/* Load external CSS in <Head> */}
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/css/tempusdominus-bootstrap-4.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/icheck-bootstrap/icheck-bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.5/jquery-jvectormap.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/admin-lte/dist/css/adminlte.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/overlayscrollbars/2.10.1/styles/overlayscrollbars.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css"
-        />
-        <link
-          href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css"
-          rel="stylesheet"
-        />
-      </Head>
-
       <div className="wrapper">
-        <h1>Hello world</h1>
+        <Header></Header>
+        {/* <Menu></Menu> */}
+        <MiniDashboard></MiniDashboard>
+        <Footer></Footer>
       </div>
-
-      {/* Load external JS files dynamically using Script component */}
-      <Script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/chart.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.5/jquery-jvectormap.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/moment/min/moment.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.39.0/js/tempusdominus-bootstrap-4.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/admin-lte/dist/js/adminlte.min.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/admin-lte/dist/js/demo.js"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://cdn.jsdelivr.net/npm/admin-lte/dist/js/pages/dashboard.js"
-        strategy="afterInteractive"
-      />
     </>
   );
 };
